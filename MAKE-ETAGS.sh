4750e6ebaf7a4d70d8c6d8860dc38a48c4903d04
Peter Sjödin <psj@kth.se>
2020-09-18 21:33:38 +0200

    add lwip

--- MAKE-ETAGS.sh
+++ MAKE-ETAGS.sh
@@ -1,4 +1,4 @@
-TAGDIRS="sys core drivers/periph_common/ drivers/at"
+TAGDIRS="sys core drivers/periph_common/ drivers/at pkg/lwip"
 TAGFILES="drivers/include/periph/uart.h drivers/include/at.h"
 #find  ${TAGDIRS} -name '*.[ch]'
 find  ${TAGDIRS} -name '*.[ch]' | etags -
