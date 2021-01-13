fc4c3fd45e1966e54bfc3627fc32009a03cfb13f
Robert Olsson <robert@herjulf.se>
2021-01-13 22:47:47 +0100

    Add Lidar commands 	modified:   tests/periph_lidar/Makefile 	modified:   tests/periph_lidar/main.c

--- tests/periph_lidar/main.c
+++ tests/periph_lidar/main.c
@@ -1,20 +1,36 @@
-#define VERSION "0.9 2021-01-13"
-
+#define VERSION "0.91 2021-01-13" /* Robert Olsson/KTH 20210-01-13 */
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-
+#include <time.h>
 #include "board.h"
 #include "shell.h"
 #include "thread.h"
 #include "msg.h"
 #include "ringbuffer.h"
 #include "periph/uart.h"
+#include "periph/rtt.h"
 #include "xtimer.h"
 
 #ifdef MODULE_STDIO_UART
 #include "stdio_uart.h"
 #endif
+
+/* Command for tf01 plus */
+#define  CMD_FW_VERSION 1
+#define  CMD_RESET      2
+#define  CMD_RATE       3
+#define  CMD_TRIGGER    4
+#define  CMD_OUT_FORMAT    5 /* Output format */
+#define  CMD_BAUD       6
+#define  CMD_OUT_DISABLE   7
+#define  CMD_COMM_MODE  0xA  /* UART or I2C */
+#define  CMD_I2C_ADDR   0xB  /* Set I2C ADDR */
+#define  CMD_I2C_READ     0  /* cm or mm */
+#define  CMD_IO_MODE   0x3B
+#define  CMD_RESTORE_FACTORY   0x10
+#define  CMD_SAVE_SETTING      0x11
+
 
 #ifndef SHELL_BUFSIZE
 #define SHELL_BUFSIZE       (128U)
@@ -35,10 +51,16 @@ typedef struct {
   ringbuffer_t rx_buf;
 } uart_ctx_t;
 
+struct {
+  uint16_t dist, signal;
+  int temp;
+  time_t time;
+} lr;
+
 static uart_ctx_t ctx[UART_NUMOF];
 static kernel_pid_t parser_pid;
 static char parser_stack[THREAD_STACKSIZE_MAIN];
-unsigned char buf[120];
+unsigned char buf[40];
 
 void wait_ms(int timeout)
 {
@@ -46,17 +68,70 @@ void wait_ms(int timeout)
   xtimer_usleep(tmo);
 }
 
-int lidar_version(int dev)
+inline uint32_t now(void)
 {
-  /* Version 5A 04 01 5F */
-  
-  if(1)  {
-    buf[0] = 0x5A;
-    buf[1] = 0x04;
-    buf[2] = 0x01;
-    buf[3] = 0x5F;
-    uart_write(UART_DEV(dev), buf, 4);
-  }
+  return rtt_get_counter();
+}
+
+int cmd_get_fw_version(int dev)
+{
+  buf[0] = 0x5A;
+  buf[1] = 0x04;
+  buf[2] = CMD_FW_VERSION;
+  buf[3] = 0x5F;
+  uart_write(UART_DEV(dev), buf, 4);
+  return 1;
+}
+
+int cmd_reset(int dev)
+{
+  buf[0] = 0x5A;
+  buf[1] = 0x04;
+  buf[2] = CMD_RESET;
+  buf[3] = 0x60;
+  uart_write(UART_DEV(dev), buf, 4);
+  return 1;
+}
+
+int cmd_set_rate(int dev, uint16_t rate)
+{
+  buf[0] = 0x5A;
+  buf[1] = 0x06;
+  buf[2] = CMD_RATE;
+  buf[3] = rate & 0xFF;
+  buf[4] = (rate << 8);
+  buf[5] = buf[0] + buf[1] + buf[3] + buf[4];
+  uart_write(UART_DEV(dev), buf, 6);
+  return 1;
+}
+
+int cmd_trigger(int dev)
+{
+  buf[0] = 0x5A;
+  buf[1] = 0x04;
+  buf[2] = CMD_TRIGGER;
+  buf[3] = 0x62;
+  uart_write(UART_DEV(dev), buf, 4);
+  return 1;
+}
+
+int cmd_restore_factory(int dev)
+{
+  buf[0] = 0x5A;
+  buf[1] = 0x04; 
+  buf[2]= CMD_RESTORE_FACTORY;
+  buf[3] = 0x6E;
+  uart_write(UART_DEV(dev), buf, 4);
+  return 1;
+}
+
+int cmd_save_setting(int dev)
+{
+  buf[0] = 0x5A;
+  buf[1] = 0x04; 
+  buf[2]= CMD_SAVE_SETTING;
+  buf[3] = 0x6F;
+  uart_write(UART_DEV(dev), buf, 4);
   return 1;
 }
 
@@ -94,21 +169,20 @@ unsigned char csum(unsigned char *in, unsigned char len)
 
 void parse_data(unsigned char *buf)
 {  
-  uint16_t dist, signal;
-  int temp;
-
   //printf("CS frame-cs=%02x csum=%02x\n", buf[8], csum(buf, 8));
   if(buf[8] != csum(buf, 8)) {
     return; // csum err
   }
   
-  dist = buf[2] + buf[3]*256;
-  signal = buf[4] + buf[5]*256;
-  temp = buf[6] + buf[7]*256;
-  temp = temp/8 -256;
-  printf("Dist=%-5u Signal=%-5u Temp=%-d", dist, signal, temp);
-  printf("\r");
-  
+  lr.dist = buf[2] + buf[3]*256;
+  lr.signal = buf[4] + buf[5]*256;
+  lr.temp = buf[6] + buf[7]*256;
+  lr.temp = lr.temp/8 -256;
+  lr.time = now();
+  if(1) {
+    printf("Dist=%-5u Signal=%-5u Temp=%-d", lr.dist, lr.signal, lr.temp);
+    printf("\r");
+  }
 #if DEBUG
   int i;
   printf("parse_data\n");
@@ -121,11 +195,41 @@ void parse_data(unsigned char *buf)
 void parse_command(unsigned char *buf)
 {
   int i;
-  printf("parse_command\n");
+  uint16_t rate;
+  uint8_t len = buf[1];
+  uint8_t cmd = buf[2];
+  
+  switch(cmd) {
 
-  for(i=0; i < 7; i++)
-    printf(" %02x", buf[i]);
-  printf("\n");
+  case CMD_RESET:
+    printf("RESET %s\n" ,buf[3]?"FAIL":"OK");
+    break;
+
+  case CMD_FW_VERSION:
+    printf("Version V%-u.%-u.%-u\n", buf[5], buf[4], buf[3]);
+    break;
+
+  case CMD_RATE:
+    rate = (((uint16_t) buf[4])<<8) + buf[3];
+    printf("Rate %-u Hz\n", rate);
+    break;
+
+  case CMD_RESTORE_FACTORY:
+    printf("RESTORE %s\n" ,buf[3]?"FAIL":"OK");
+    break;
+
+  case CMD_SAVE_SETTING:
+    printf("SAVE SETTING %s\n" ,buf[3]?"FAIL":"OK");
+    break;
+    
+  default:
+    printf("parse_command: ");
+    for(i=0; i < len; i++)
+      printf(" %02x", buf[i]);
+    printf("\n");
+
+    break;
+  }
 }
 
 static void *parser(void *arg)
@@ -143,8 +247,10 @@ static void *parser(void *arg)
       continue;
 
     i = 0;
-    while  (! ringbuffer_empty(&(ctx[dev].rx_buf))) {
+    while (! ringbuffer_empty(&(ctx[dev].rx_buf))) {
       buf[i++] = (int)ringbuffer_get_one(&(ctx[dev].rx_buf));
+      if(i == (sizeof(buf)-1))
+	break;
       xtimer_usleep(100); // Investigation needed */
     }
 
@@ -158,7 +264,7 @@ static void *parser(void *arg)
   return NULL;
 }
 
-static int init(int dev, uint32_t baud)
+static int init_serial(int dev, uint32_t baud)
 {
   int res;
     
@@ -189,13 +295,13 @@ int main(void)
 {
   uint32_t baud = 115200L;
   unsigned dev = 1;
-
-  printf("Version %s\n", VERSION);
+  uint16_t rate = 10; /* Hz 1-1000 */
   
-  printf("Available devices:               %i\n", UART_NUMOF);
+  printf("Version %s\n", VERSION);
+  printf("Number of UART: %i\n", UART_NUMOF);
 
   if (STDIO_UART_DEV != UART_UNDEF) {
-    printf("UART used for STDIO (the shell): UART_DEV(%i)\n\n", STDIO_UART_DEV);
+    printf("UART used for STDIO: UART_DEV(%i)\n\n", STDIO_UART_DEV);
   }
 
   /* initialize ringbuffers */
@@ -203,12 +309,20 @@ int main(void)
     ringbuffer_init(&(ctx[i].rx_buf), ctx[i].rx_mem, UART_BUFSIZE);
   }
 
+  init_serial(dev, baud);
+
+  cmd_reset(dev);
+  wait_ms(10);
+  cmd_get_fw_version(dev);
+  wait_ms(10);
+  cmd_set_rate(dev, rate);
+  wait_ms(10);
+  cmd_restore_factory(dev);
+  cmd_save_setting(dev);    
+
   /* start the parser thread */
   parser_pid = thread_create(parser_stack, sizeof(parser_stack),
 			      PARSER_PRIO, 0, parser, NULL, "parser");
-  init(dev, baud);
-  lidar_version(dev);
-    
   /* run the shell */
   char line_buf[SHELL_BUFSIZE];
   shell_run(shell_commands, line_buf, SHELL_BUFSIZE);
